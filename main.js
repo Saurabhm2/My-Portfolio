3f6315889b432f72ea259e846eb9cec84663d450
Saurabh Mahamuni <109416515+Saurabhm2@users.noreply.github.com>
2024-06-02 17:38:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Software Engineer", "YouTuber", "Full Stack Developer", "Blogger"], // Corrected 'String' to 'strings'
+    strings: ["Full Stack Web Developer", "Graphic Designer", "Blogger", "YouTuber"], // Corrected 'String' to 'strings'
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
