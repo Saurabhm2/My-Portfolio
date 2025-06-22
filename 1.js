a1303c370900687c945da81faadaf7c305a51190
Saurabh Mahamuni <109416515+Saurabhm2@users.noreply.github.com>
2025-06-22 15:00:23 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -9,12 +9,38 @@ $(document).ready(function() {
 });
 
 $(document).ready(function(){
-    // This code runs when the document (HTML) has finished loading
-
+    // Enhanced mobile navigation
     $('.navbar-toggle').click(function(){
-        // This code runs when the '.navbar-toggle' button is clicked
+        $('.navbar').toggleClass('active');
+        $('#menu-icon').toggleClass('bx-menu bx-x');
+    });
 
-        console.log("Navbar toggle button clicked"); // Log a message to the console
-        $('.navbar').toggleClass('active'); // Toggle the 'active' class on the '.navbar' element
+    // Close navbar when clicking on a link (mobile)
+    $('.navbar a').click(function(){
+        if($(window).width() <= 768) {
+            $('.navbar').removeClass('active');
+            $('#menu-icon').removeClass('bx-x').addClass('bx-menu');
+        }
+    });
+
+    // Close navbar when clicking outside (mobile)
+    $(document).click(function(e) {
+        if (!$(e.target).closest('.header').length) {
+            $('.navbar').removeClass('active');
+            $('#menu-icon').removeClass('bx-x').addClass('bx-menu');
+        }
+    });
+
+    // Smooth scrolling enhancement
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var target = this.hash;
+        var $target = $(target);
+
+        if($target.length) {
+            $('html, body').animate({
+                'scrollTop': $target.offset().top - 80
+            }, 800, 'swing');
+        }
     });
 });
